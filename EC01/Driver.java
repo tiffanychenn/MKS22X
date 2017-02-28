6d996c2b5651a80aa10a54bb82d08dc473931357
Tiffany Chen <tiffanysta.c@gmail.com>
2017-02-28 07:56:38 -0500

    tested stuff

--- EC01/Driver.java
+++ EC01/Driver.java
@@ -1,13 +1,16 @@
 public class Driver{
 
     public static void main(String[] args){
-    	for (int i = 0; i < 8; i ++){
+    	/*for (int i = 0; i < 8; i ++){
     		for (int j = 0; j < 8; j ++){
     			KnightBoard k = new KnightBoard(i, j);
     			k.solve();
     			System.out.println(i); System.out.println(j); System.out.println(k);
     		}
-    	}
+    	}*/
+    	KnightBoard k = new KnightBoard(5, 5);
+    	k.solve();
+    	System.out.println(k);
     }
 
 }
