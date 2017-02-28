122277e1f410379123796a48f54f527617c9dc6e
Tiffany Chen <tiffanysta.c@gmail.com>
2017-02-27 20:16:54 -0500

    checked regular solve

--- EC01/Driver.java
+++ EC01/Driver.java
@@ -1,10 +1,12 @@
 public class Driver{
 
     public static void main(String[] args){
-    	for (int i = 40; i < 61; i ++){
-    		KnightBoard k = new KnightBoard(i, i);
-    		k.solveFast();
-    		System.out.println(i); System.out.println(k);
+    	for (int i = 0; i < 8; i ++){
+    		for (int j = 0; j < 8; j ++){
+    			KnightBoard k = new KnightBoard(i, j);
+    			k.solve();
+    			System.out.println(i); System.out.println(j); System.out.println(k);
+    		}
     	}
     }
 
