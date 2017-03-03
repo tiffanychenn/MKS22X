a60268588e31023a9d241854a841372ec4350f6e
Tiffany Chen <tiffanysta.c@gmail.com>
2017-03-03 06:44:31 -0500

    finished code

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -21,33 +21,57 @@ public class Maze{
 
     public Maze(String filename){
         readFile(filename);
-	animate = false;
+        animate = false;
+        boolean e = false, s = false;
+        for (int i = 0; i < maze.length; i ++){
+            for (int j = 0; j < maze[0].length; j ++){
+                if (maze[i][j] == 'E'){
+                    e = true;
+                }
+                if (maze[i][j] == 'S'){
+                    s = true;
+                }
+            }
+        }
+        if (!(s && e)){
+            throw new IllegalArgumentException("file must contain a start (S) and an end (E) of the maze.");
+        }
     }
 
     private void readFile(String file){
-	try {
-	    Scanner sc = new Scanner(new File(file));
-	    sc.useDelimiter("\n");
-	    int counter = 0;
-	    while (sc.hasNext()){
-		char[] ary = new char[sc.next().length()];
-	        for (int i = 0; i < sc.next().length(); i ++){
-		    ary[i] = sc.next.charAt(i);
-		}
-		counter ++;
-	    }
-	}
-	catch (Exception e){
-	    System.out.println("ugh");
-	}
+        try {
+            Scanner sc = new Scanner(new File(file));
+            int counter = 1;
+            char[][] temp = new char[100][sc.nextLine().length()];
+            for (int i = 0; i < temp[0].length; i ++){
+                temp[0][i] = '#';
+            }
+            while (sc.hasNextLine()){
+                char[] ary = sc.nextLine().toCharArray();
+                temp[counter] = ary;
+                counter ++;
+            }
+            for (int i = 1; i < temp.length; i ++){
+                if (!(temp[i][0] == ' ' || temp[i][0] == '#' || temp[i][0] == 'S' || temp[i][0] == 'E')){
+                    maze = new char[i][temp[0].length];
+                    break;
+                }
+            }
+            for (int i = 0; i < maze.length; i ++){
+                maze[i] = temp[i];
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+            System.exit(1);
+        }
     }
     
     private void wait(int millis){ //ADDED SORRY!
-	try {
-	    Thread.sleep(millis);
-	}
-	catch (InterruptedException e) {
-	}
+        try {
+            Thread.sleep(millis);
+        }
+        catch (InterruptedException e){}
     }
 
     public void setAnimate(boolean b){
@@ -59,16 +83,21 @@ public class Maze{
         System.out.println("\033[2J\033[1;1H");
     }
 
-    /*Wrapper Solve Function
-      Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
+    /* Wrapper Solve Function
+       Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
     */
     public boolean solve(){
-            int startr=-1,startc=-1;
-
-            //Initialize starting row and startint col with the location of the S. 
-
-            maze[startr][startc] = ' ';//erase the S, and start solving!
-            return solve(startr,startc);
+        int startr = -1, startc = -1;
+        for (int i = 0; i < maze.length; i ++){
+            for (int j = 0; j < maze[0].length; j ++){
+                if (maze[i][j] == 'S'){
+                    startr = i;
+                    startc = j;
+                }
+            }
+        }
+        maze[startr][startc] = ' ';
+        return solve(startr, startc);
     }
 
     /*
@@ -79,24 +108,57 @@ public class Maze{
       Returns true when the maze is solved,
       Returns false when the maze has no solution.
 
-
       Postcondition:
 
-        The S is replaced with '@' but the 'E' is not.
+      The S is replaced with '@' but the 'E' is not.
 
-        All visited spots that were not part of the solution are changed to '.'
-        All visited spots that are part of the solution are changed to '@'
+      All visited spots that were not part of the solution are changed to '.'
+      All visited spots that are part of the solution are changed to '@'
     */
-    private boolean solve(int row, int col){
-        if(animate){
-            System.out.println("\033[2J\033[1;1H"+this);
 
+    private boolean solve(int row, int col){
+        if (animate){
+            System.out.println("\033[2J\033[1;1H" + this);
             wait(20);
         }
+        if (maze[row][col] == 'E'){
+            return true;
+        }
+        if (maze[row][col] != ' '){
+            return false;
+        }
+        maze[row][col] = '@';
+        if (!(solve(row + 1, col) || solve(row - 1, col) || solve(row, col + 1) || solve(row, col - 1))){
+            maze[row][col] = '.';
+            if (maze[row + 1][col] == '@'){
+                maze[row + 1][col] = ' ';
+                solve(row + 1, col);
+            }
+            else if (maze[row - 1][col] == '@'){
+                maze[row - 1][col] = ' ';
+                solve(row - 1, col);
+            }
+            else if (maze[row][col + 1] == '@'){
+                maze[row][col + 1] = ' ';
+                solve(row, col + 1);
+            }
+            else if (maze[row][col - 1] == '@'){
+                maze[row][col - 1] = ' ';
+                solve(row, col - 1);
+            }
+        }
+        return true;
+    }
 
-        //COMPLETE SOLVE
-
-        return false; //so it compiles
+    public String toString(){
+        String s = "";
+        for (int i = 0; i < maze.length; i ++){
+            for (int j = 0; j < maze[0].length; j ++){
+                s += maze[i][j];
+            }
+            s += "\n";
+        }
+        return s;
     }
 
 
