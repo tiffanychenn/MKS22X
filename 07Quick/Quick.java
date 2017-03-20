799c8a8aafbaef07e9f5da171e8e9ba8bacc9d2a
Tiffany Chen <tiffanysta.c@gmail.com>
2017-03-20 19:35:42 -0400

    finished optimized quicksort

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class Quick{
 
+    private static void swap (int[] ary, int i, int j){
+        int tempy = ary[i];
+        ary[i] = ary[j];
+        ary[j] = tempy;
+    }
+
     public static int part(int[] ary, int start, int end){
         Random rand = new Random();
         int r = rand.nextInt(end - start) + start;
@@ -9,38 +15,17 @@ public class Quick{
         int compare = ary[r];
         int counterS = start;
         int counterE = end - 1;
-        int tempy = ary[end];
-        ary[end] = compare;
-        ary[r] = tempy;
-        while (counterS != counterE && counterE != counterS + 1 && counterE != counterS - 1){
-            if (ary[counterS] > compare && ary[counterE] < compare){
-                tempy = ary[counterE];
-                ary[counterE] = ary[counterS];
-                ary[counterS] = tempy;
-                counterE --;
+        swap(ary, end, r);
+        while (counterS <= counterE){
+            if (ary[counterS] < compare){
                 counterS ++;
             }
-            else if (ary[counterS] > compare) counterE --;
-            else if (ary[counterE] < compare) counterS ++;
             else {
+                swap(ary, counterS, counterE);
                 counterE --;
-                counterS ++;
-            }
-            //System.out.println(toString(ary));
-        }
-        if (counterE != counterS){
-            if (ary[counterS] > ary[counterE]){
-                tempy = ary[counterE];
-                ary[counterE] = ary[counterS];
-                ary[counterS] = tempy;
             }
         }
-        while (ary[counterS] < compare){
-            counterS ++;
-        }
-        tempy = ary[counterS];
-        ary[counterS] = compare;
-        ary[end] = tempy;
+        swap(ary, end, counterS);
         return counterS;
     }
 
@@ -49,7 +34,7 @@ public class Quick{
     }
 
     private static int quickselectH(int[] data, int k, int start, int end){
-        if (end == start) return data[end];
+        if (end <= start) return data[end];
         int pivot = part(data, start, end);
         if (pivot == k) return data[pivot];
         else if (pivot > k) return quickselectH(data, k, 0, pivot - 1);
@@ -62,21 +47,39 @@ public class Quick{
 
     private static void quicksortH(int[] data, int start, int end){
         if (start < end){
-            int pivot = part(data, start, end);
-            quicksortH(data, start, pivot - 1);
-            quicksortH(data, pivot + 1, end);
+            Random rand = new Random();
+            int r = rand.nextInt(end - start) + start;
+            if (r == start) r ++;
+            int v = data[r];
+            swap(data, start, r);
+            int i = start;
+            int lt = start + 1;
+            int gt = end;
+            while (i <= gt){
+                if (data[i] == v){
+                    i ++;
+                }
+                else if (data[i] > v){
+                    swap(data, i, gt);
+                    gt --;
+                }
+                else {
+                    swap(data, lt, i);
+                    lt ++; i ++;
+                }
+            }
+            quicksortH(data, start, lt - 1);
+            quicksortH(data, gt + 1, end);
         }
     }
 
     public static void main(String[] args){
-        int[] ary = {2, 10, 15, 23, 0, 5};
-        quicksort(ary);
-        System.out.println(toString(ary));
-        //System.out.println(quickselect(ary, 0)); //would return 0
-        //System.out.println(quickselect(ary, 1)); //would return 2
-        //System.out.println(quickselect(ary, 2)); //would return 5
-        //System.out.println(quickselect(ary, 3)); //would return 10
-        //System.out.println(quickselect(ary, 4)); //would return 15
+        int[] ary = {5, 10, 0, 15, 2};
+        System.out.println(quickselect(ary, 0)); //would return 0
+        System.out.println(quickselect(ary, 1)); //would return 2
+        System.out.println(quickselect(ary, 2)); //would return 5
+        System.out.println(quickselect(ary, 3)); //would return 10
+        System.out.println(quickselect(ary, 4)); //would return 15
     }
 
     public static String toString(int[] ary){
