0bfd9d03c7a81fe85e59ab3255559cf19d98eb1e
Tiffany Chen <tiffanysta.c@gmail.com>
2017-03-30 15:01:07 -0400

    finished some of the phase 2 problems

--- 09LinkedList/Driver.java
+++ 09LinkedList/Driver.java
@@ -2,15 +2,14 @@ public class Driver{
 
 	public static void main(String[] args){
 		MyLinkedList l = new MyLinkedList();
-		System.out.println(l);
 		l.add(5);
-		System.out.println(l);
 		l.add(10);
-		System.out.println(l);
 		l.add(47);
-		l.add(36817);
 		System.out.println(l);
-		System.out.println(l.size());
+		l.add(1, 36817);
+		System.out.println(l);
+		l.add(2, 19);
+		System.out.println(l);
 	}
 
 }
--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -14,10 +14,6 @@ public class MyLinkedList{
             next = n;
         }
 
-        private void changeRef(Node neww){
-            next = neww;
-        }
-
     }
 
     private Node start;
@@ -38,9 +34,28 @@ public class MyLinkedList{
         while (current.next != null){
             current = current.next;
         }
-        current.changeRef(new Node(e));
+        current.next = new Node(e);
         size ++;
         return true;
+    }
+
+    public void add(int index, int value){
+        if (index >= size) throw new IndexOutOfBoundsException();
+        int counter = 0;
+        Node current = start;
+        while (counter < index){
+            current = current.next;
+            counter ++;
+        }
+        current = new Node(value, current);
+        counter = 0;
+        Node currentN = start;
+        while (counter < index - 1){
+            currentN = currentN.next;
+            counter ++;
+        }
+        currentN.next = current;
+        size ++;
     }
 
     public int size(){
@@ -51,11 +66,44 @@ public class MyLinkedList{
         String s = "[";
         Node current = start;
         for (int i = 0; i < size; i ++){
-            s = s + current.element + " ";
+            s = s + current.element + ", ";
             current = current.next;
         }
         if (size == 0) return s + "]";
-        return s.substring(0, s.length() - 1) + "]";
+        return s.substring(0, s.length() - 2) + "]";
+    }
+
+    public int get(int index){
+        if (index >= size) throw new IndexOutOfBoundsException();
+        int counter = 0;
+        Node current = start;
+        while (counter < index){
+            current = current.next;
+            counter ++;
+        }
+        return current.element;
+    }
+
+    public void set(int index, int value){
+        if (index >= size) throw new IndexOutOfBoundsException();
+        int counter = 0;
+        Node current = start;
+        while (counter < index){
+            current = current.next;
+            counter ++;
+        }
+        current.element = value;
+    }
+
+    public int indexOf(int value){
+        int counter = 0;
+        Node current = start;
+        while (current.element != value){
+            if (current.next == null) return -1;
+            current = current.next;
+            counter ++;
+        }
+        return counter;
     }
 
 }
