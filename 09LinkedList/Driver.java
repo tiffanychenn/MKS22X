c296813b1ac866d7d87700b54f3ec04e2437356b
Tiffany Chen <tiffanysta.c@gmail.com>
2017-04-04 14:50:53 -0400

    wokred on double linked list

--- 09LinkedList/Driver.java
+++ 09LinkedList/Driver.java
@@ -6,14 +6,14 @@ public class Driver{
 		l.add(10);
 		l.add(47);
 		System.out.println(l);
-		l.add(1, 36817);
+		/*l.add(1, 36817);
 		System.out.println(l);
 		l.add(2, 19);
 		System.out.println(l);
 		l.remove(2);
 		System.out.println(l);
 		l.remove(1);
-		System.out.println(l);
+		System.out.println(l);*/
 	}
 
 }
--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -1,120 +1,93 @@
+import java.util.*;
+
 public class MyLinkedList{
 
     private class Node{
 
-        private int element;
-        private Node next;
+        Node next, prev;
+        int value;
 
-        private Node(int e){
-            element = e;
+        private Node(int v){
+          value = v;
         }
 
-        private Node(int e, Node n){
-            element = e;
+        private Node(int v, Node n){
+            value = v;
             next = n;
         }
 
     }
 
-    private Node start;
-    private int size;
+    Node start, end;
+    int size;
 
     public MyLinkedList(){}
 
-    public boolean add(int e){
-        if (size == 0){
-            start = new Node(e);
-            size ++;
-            return true;
-        }
-        Node current = start;
-        while (current.next != null){
-            current = current.next;
-        }
-        current.next = new Node(e);
-        size ++;
-        return true;
-    }
-
-    public void add(int index, int value){
-        if (index >= size) throw new IndexOutOfBoundsException();
-        int counter = 0;
-        Node current = start;
-        while (counter < index){
-            current = current.next;
-            counter ++;
-        }
-        current = new Node(value, current);
-        counter = 0;
-        Node currentN = start;
-        while (counter < index - 1){
-            currentN = currentN.next;
-            counter ++;
-        }
-        currentN.next = current;
-        size ++;
-    }
-
     public int size(){
         return size;
+    }
+
+    private Node getNthNode(int n){
+        if (n >= size) throw new IndexOutOfBoundsException();
+        Node current;
+        if (n > size / 2){
+            int counter = size - n;
+            current = end;
+            while (counter > 0){
+                counter --;
+                current = current.prev;
+            }
+        }
+        else {
+            int counter = n;
+            current = start;
+            while (counter > 0){
+                counter --;
+                current = current.next;
+            }
+        }
+        return current;
+    }
+
+    private void remove(Node target){
+        target.prev.next = target.next;
+        target.next.prev = target.prev;
+        size --;
     }
 
     public String toString(){
         String s = "[";
         Node current = start;
         for (int i = 0; i < size; i ++){
-            s = s + current.element + ", ";
+            s += current.value + " ";
             current = current.next;
+
         }
-        if (size == 0) return s + "]";
-        return s.substring(0, s.length() - 2) + "]";
+        if (size == 0){
+            return s + "]";
+        }
+        return s.substring(0, s.length() - 1) + "]";
     }
 
-    public int get(int index){
-        if (index >= size) throw new IndexOutOfBoundsException();
-        int counter = 0;
-        Node current = start;
-        while (counter < index){
-            current = current.next;
-            counter ++;
+    public boolean add(int value){
+        if (size == 0){
+            start = new Node(value, end);
         }
-        return current.element;
+        else {
+            end = new Node(value, end);
+        }
+        size ++;
+        return true;
     }
 
-    public void set(int index, int value){
-        if (index >= size) throw new IndexOutOfBoundsException();
-        int counter = 0;
-        Node current = start;
-        while (counter < index){
-            current = current.next;
-            counter ++;
-        }
-        current.element = value;
-    }
+    //public int get(int index){/*complete this*/}
+    
+    //public int set(int index, int value){/*complete this*/}
 
-    public int indexOf(int value){
-        int counter = 0;
-        Node current = start;
-        while (current.element != value){
-            if (current.next == null) return -1;
-            current = current.next;
-            counter ++;
-        }
-        return counter;
-    }
+    //public int indexOf(int value){/*complete this*/}
 
-    public int remove(int index){
-        if (index >= size) throw new IndexOutOfBoundsException();
-        int counter = 0;
-        Node current = start;
-        while (counter < index - 1){
-            current = current.next;
-            counter ++;
-        }
-        Node now = current.next.next;
-        current.next = now;
-        size --;
-        return index;
-    }
+    //public int remove(int index){/*complete this*/}
+
+    //public void add(int index,int value){/*complete this*/}
 
 }
