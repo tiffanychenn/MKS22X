b223d05a7fdf3296a72a5cb91e0a5b22ee5db4c3
Tiffany Chen <tiffanysta.c@gmail.com>
2017-04-05 15:03:18 -0400

    finished the basic methods of double linked lists

--- 09LinkedList/Driver.java
+++ 09LinkedList/Driver.java
@@ -5,15 +5,17 @@ public class Driver{
 		l.add(5);
 		l.add(10);
 		l.add(47);
-		System.out.println(l);
-		/*l.add(1, 36817);
+		l.add(287);
+		l.add(3);
+		l.add(486);
+		l.add(1, 36817);
 		System.out.println(l);
 		l.add(2, 19);
 		System.out.println(l);
-		l.remove(2);
+		l.add(0, 5);
 		System.out.println(l);
-		l.remove(1);
-		System.out.println(l);*/
+		l.add(l.size() - 1, 1368798);
+		System.out.println(l);
 	}
 
 }
--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -13,7 +13,14 @@ public class MyLinkedList{
 
         private Node(int v, Node n){
             value = v;
-            next = n;
+            prev = n;
+        }
+
+        public String toString(){
+            if (prev == null && next == null) return "null " + value + " null";
+            else if (prev == null) return "null " + value + " " + next.value;
+            else if (next == null) return prev.value + " " + value + " null";
+            return prev.value + " " + value + " " + next.value;
         }
 
     }
@@ -33,7 +40,7 @@ public class MyLinkedList{
         if (n > size / 2){
             int counter = size - n;
             current = end;
-            while (counter > 0){
+            while (counter > 1){
                 counter --;
                 current = current.prev;
             }
@@ -71,23 +78,82 @@ public class MyLinkedList{
 
     public boolean add(int value){
         if (size == 0){
-            start = new Node(value, end);
+            start = new Node(value);
+        }
+        else if (size == 1){
+            end = new Node(value);
+            end.prev = start;
+            start.next = end;
         }
         else {
             end = new Node(value, end);
+            end.prev.next = end;
         }
         size ++;
         return true;
     }
 
-    //public int get(int index){/*complete this*/}
+    public int get(int index){
+        Node n = getNthNode(index);
+        return n.value;
+    }
     
-    //public int set(int index, int value){/*complete this*/}
+    public int set(int index, int value){
+        Node n = getNthNode(index);
+        int v = n.value;
+        n.value = value;
+        return v;
+    }
 
-    //public int indexOf(int value){/*complete this*/}
+    public int indexOf(int value){
+        int counter = 0;
+        Node current = start;
+        while (current != null && current.value != value){
+            counter ++;
+            current = current.next;
+        }
+        if (counter == size) return -1;
+        return counter;
+    }
 
-    //public int remove(int index){/*complete this*/}
+    public int remove(int index){
+        Node n = getNthNode(index);
+        if (index == 0){
+            n.next.prev = null;
+            start = n.next;
+        }
+        else if (index == size - 1){
+            n.prev.next = null;
+            end = n.prev;
+        }
+        else {
+            n.prev.next = n.next;
+            n.next.prev = n.prev;
+        }
+        size --;
+        return n.value;
+    }
 
-    //public void add(int index,int value){/*complete this*/}
+    public void add(int index, int value){
+        Node n = getNthNode(index);
+        Node p = new Node(value);
+        if (index == 0){
+            start = p;
+            p.next = n;
+            n.prev = p;
+        }
+        else if (index == size - 1){
+            end = p;
+            p.prev = n;
+            n.next = p;
+        }
+        else {
+            p.prev = n.prev;
+            p.next = n;
+            n.prev.next = p;
+            n.prev = p;
+        }
+        size ++;
+    }
 
 }
